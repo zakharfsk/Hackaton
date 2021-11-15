906b23e93d41983d38f360c3e24ffd38778d8f40
ZaharFsk <68950796+xzartsust@users.noreply.github.com>
2021-11-16 00:06:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import eel
+import time
+
+sec = 0
+
+@eel.expose
+def get_time(time):
+    print(time)
+
+@eel.expose
+def timer(time):
+    return time + 1
+
+eel.init('web')
+eel.start('index.html', size = (700, 700), block = False)
