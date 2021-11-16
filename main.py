8276d9b73aa4652e575b33f7aa452bff248a9f63
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-16 10:13:05 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import eel
-import time
 
-sec = 0
+settings = {
+    'mode': "chrome", #or "chrome-app",
+    'host': 'localhost',
+    'port': 8080,
+    'chromeFlags': ["--start-fullscreen", "--browser-startup-dialog"]
+}
 
 @eel.expose
 def get_time(time):
@@ -12,4 +16,4 @@ def timer(time):
     return time + 1
 
 eel.init('web')
-eel.start('index.html', size = (700, 700))
+eel.start('index.html', size = (700, 700), mode = 'chrome', cmdline_args = ['--browser-startup-dialog'])
