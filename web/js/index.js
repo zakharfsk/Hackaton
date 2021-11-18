1ef7eb666916762fd670ad6943f28a4f2ca87e7b
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-11-19 00:24:41 +0200

    bigs update

---
+++ web/js/index.js
@@ -0,0 +1,30 @@
+let body = document.querySelector("body"),
+    bottomScroll = document.querySelector(".scroll"),
+    upScroll = document.querySelector(".upscroll"),
+    commentsSection = document.querySelector(".book-article");
+
+bottomScroll.onclick = scrollBottom;
+upScroll.onclick = scrollTop;
+
+function scrollBottom(){
+    window.scrollTo(0, window.pageYOffset + 10);
+
+    if(window.pageYOffset >= commentsSection.offsetTop){
+        return false;
+    }else{
+        setTimeout(()=>{
+            scrollBottom();
+        },1)
+    }
+}
+function scrollTop(){
+    window.scrollTo(0, window.pageYOffset - 5);
+
+    if(window.pageYOffset <= 0){
+        return false;
+    }else{
+        setTimeout(()=>{
+            scrollTop();
+        },1)
+    }
+}
